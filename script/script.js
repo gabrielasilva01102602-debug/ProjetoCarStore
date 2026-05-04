05b336262428d309075bd035e5717b0c153e5d98
gabrielasilva01102602-debug <gabriela.silva01102602@sesisenaipr.org.br>
2026-05-04 10:52:06 -0300

    .

---
+++ script/script.js
@@ -0,0 +1,8 @@
+function salvarCarros(event){
+    event.preventDefault();
+
+    let titulo = document.getElementById('title').value;
+    let titulo = document.getElementById('preco').value;
+    let titulo = document.getElementById('marca').value;
+    let titulo = document.getElementById('modelo').value;
+}
