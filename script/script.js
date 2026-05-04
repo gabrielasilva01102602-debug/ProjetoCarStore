0eb2ec71766268c8df6c673fd48933625d12052c
gabrielasilva01102602-debug <gabriela.silva01102602@sesisenaipr.org.br>
2026-05-04 11:04:28 -0300

    CarStore

--- script/script.js
+++ script/script.js
@@ -2,7 +2,9 @@ function salvarCarros(event){
     event.preventDefault();
 
     let titulo = document.getElementById('title').value;
-    let titulo = document.getElementById('preco').value;
-    let titulo = document.getElementById('marca').value;
-    let titulo = document.getElementById('modelo').value;
+    let preco = document.getElementById('preco').value;
+    let marca = document.getElementById('marca').value;
+    let modelo = document.getElementById('modelo').value;
+
+    
 }
